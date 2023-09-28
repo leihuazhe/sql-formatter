9fa91998485bb26c187b9eefda9fed44309b0bc6
leihuazhe <leihuazhe@sensorsdata.cn>
2023-09-28 15:52:23 +0800

    完善sql format

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,26 +1,5 @@
 
-:
-
-www.pc.css,6/f/6f610a91d01757e439ca4bf318ac8d4fadc7bd82
-8
-card.css,8/4/84b11e6531a5d7f4b8f1e90c926d9efd94e19f2e
-:
-
-common.css,5/d/5db796b338afba355ef805d024ec3e62501d72e9
-:
-
-footer.css,8/4/84059632c38d0952f1da786ee066ec8b73c98ebd
-9
-	index.css,6/7/674effb85dac479ec2f277de2d8e1a362611e177
-@
-index.global.css,2/e/2e19460d6eb4e4a620ae2b6fb906a189fffb7382
-:
-
-layout.css,9/4/94efb9694094e17d097ca7dd7fba4fa842f7232c
-9
-	layui.css,0/b/0b2b026ff2b6e1d622e0aa78bf91cb24f3f5af21
-;
-message.css,b/3/b3adf9c9e366ba102655dd40854743727157147c
-:
-
-mobile.css,7/f/7f5b9ac605c2f274a30e6935a29ff7806168d6bd
+F
+prettier-sql-clean.svg,4/3/43f4853c7294e0c71c5c85c95ebc6900ead1a548
+G
+js/sql-formatter.min.js,5/6/56dd3a217615eae6a13c0ac3c720a53f5b442bc1
--- js/index.js
+++ js/index.js
@@ -1,88 +1,133 @@
 const attachFormat = () => {
-  const input = document.getElementById('input');
-  const output = document.getElementById('output');
-  const error = document.getElementById('error');
+    const input = document.getElementById('input');
+    const output = document.getElementById('output');
+    const output2 = document.getElementById('output2');
+    const error = document.getElementById('error');
 
-  const language = document.getElementById('language');
-  const tabWidth = document.getElementById('tabWidth');
-  const useTabs = document.getElementById('useTabs');
-  const keywordCase = document.getElementById('keywordCase');
-  const indentStyle = document.getElementById('indentStyle');
-  const logicalOperatorNewline = document.getElementById('logicalOperatorNewline');
-  const tabulateAlias = document.getElementById('tabulateAlias');
-  const commaPosition = document.getElementById('commaPosition');
-  const expressionWidth = document.getElementById('expressionWidth');
-  const lineBetweenQueries = document.getElementById('lineBetweenQueries');
-  const denseOperators = document.getElementById('denseOperators');
-  const newlineBeforeSemicolon = document.getElementById('newlineBeforeSemicolon');
+    const language = document.getElementById('language');
+    const tabWidth = document.getElementById('tabWidth');
+    const useTabs = document.getElementById('useTabs');
+    const keywordCase = document.getElementById('keywordCase');
+    const indentStyle = document.getElementById('indentStyle');
+    const logicalOperatorNewline = document.getElementById('logicalOperatorNewline');
+    const tabulateAlias = document.getElementById('tabulateAlias');
+    const commaPosition = document.getElementById('commaPosition');
+    const expressionWidth = document.getElementById('expressionWidth');
+    const lineBetweenQueries = document.getElementById('lineBetweenQueries');
+    const denseOperators = document.getElementById('denseOperators');
+    const newlineBeforeSemicolon = document.getElementById('newlineBeforeSemicolon');
 
-  function showOutput(text) {
-    output.value = text;
-    output.style.display = 'block';
-    error.style.display = 'none';
-  }
-
-  function showError(text) {
-    error.innerHTML = text;
-    output.style.display = 'none';
-    error.style.display = 'block';
-  }
-
-  function format() {
-    try {
-      const config = {
-        language: language.options[language.selectedIndex].value,
-        tabWidth: tabWidth.value,
-        useTabs: useTabs.checked,
-        keywordCase: keywordCase.options[keywordCase.selectedIndex].value,
-        indentStyle: indentStyle.options[indentStyle.selectedIndex].value,
-        logicalOperatorNewline:
-          logicalOperatorNewline.options[logicalOperatorNewline.selectedIndex].value,
-        tabulateAlias: tabulateAlias.checked,
-        commaPosition: commaPosition.options[commaPosition.selectedIndex].value,
-        expressionWidth: expressionWidth.value,
-        lineBetweenQueries: lineBetweenQueries.value,
-        denseOperators: denseOperators.checked,
-        newlineBeforeSemicolon: newlineBeforeSemicolon.checked,
-      };
-      showOutput(sqlFormatter.format(input.value, config));
-    } catch (e) {
-      if (e instanceof sqlFormatter.ConfigError) {
-        showError(`<h2>Configuration error</h2><p>${e.message}</p>`);
-      } else {
-        showError(
-          `
-<h2>An Unexpected Error Occurred</h2>
-<p><strong>${e.message}</strong></p>
-<p>
-  Please report this at
-  <a href="https://github.com/sql-formatter-org/sql-formatter/issues">Github issues page.<a>
-</p>
-<p>Stack Trace:</p>
-<pre>${e.stack.toString()}</pre>
-`
-        );
-      }
+    function showOutput(text) {
+        output.value = text;
+        output.style.display = 'block';
+        error.style.display = 'none';
     }
-  }
 
-  input.addEventListener('input', format);
-  [
-    language,
-    tabWidth,
-    useTabs,
-    keywordCase,
-    indentStyle,
-    logicalOperatorNewline,
-    tabulateAlias,
-    commaPosition,
-    expressionWidth,
-    lineBetweenQueries,
-    denseOperators,
-    newlineBeforeSemicolon,
-  ].forEach(option => option.addEventListener('change', format));
+    function showOutput2(text) {
+        const htmlText = escapeTextToHtml(text);
+        //console.log(htmlText)
+        output2.innerHTML = htmlText;
+        output2.style.display = 'block';
+        output.style.display = 'none';
+        error.style.display = 'none';
+    }
 
-  format();
+    function escapeTextToHtml(text) {
+        return text
+            .replaceAll('\n', '<br/>')
+            .replaceAll('\t', "&nbsp;");
+    }
+
+    var stringToHTML = function (str) {
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(str, 'text/html');
+        return doc.body;
+    };
+
+
+    function showError(text) {
+        error.innerHTML = text;
+        output.style.display = 'none';
+        error.style.display = 'block';
+    }
+
+    function format() {
+        try {
+            // const config = {
+            //     language: language.options[language.selectedIndex].value,
+            //     tabWidth: tabWidth.value,
+            //     useTabs: useTabs.checked,
+            //     keywordCase: keywordCase.options[keywordCase.selectedIndex].value,
+            //     indentStyle: indentStyle.options[indentStyle.selectedIndex].value,
+            //     logicalOperatorNewline:
+            //     logicalOperatorNewline.options[logicalOperatorNewline.selectedIndex].value,
+            //     tabulateAlias: tabulateAlias.checked,
+            //     commaPosition: commaPosition.options[commaPosition.selectedIndex].value,
+            //     expressionWidth: expressionWidth.value,
+            //     lineBetweenQueries: lineBetweenQueries.value,
+            //     denseOperators: denseOperators.checked,
+            //     newlineBeforeSemicolon: newlineBeforeSemicolon.checked,
+            // };
+
+            const config = {
+                language: 'sql',
+                tabWidth: 2,
+                useTabs: false,
+                keywordCase: 'upper',
+                indentStyle: 'standard',
+                logicalOperatorNewline: 'before',
+                tabulateAlias: true,
+                commaPosition: 'after',
+                expressionWidth: "50",
+                lineBetweenQueries: "1",
+                denseOperators: false,
+                newlineBeforeSemicolon: false,
+            };
+            console.log(config)
+
+            var formatValue = sqlFormatter.format(input.value, config);
+            formatValue = formatValue.replaceAll(' ', '\t')
+            const hl = hljs.highlight(formatValue, {language: 'sql'}).value;
+            showOutput2(hl);
+        } catch (e) {
+            if (e instanceof sqlFormatter.ConfigError) {
+                showError(`<h2>Configuration error</h2><p>${e.message}</p>`);
+            } else {
+//                 showError(
+//                     `
+// <h2>An Unexpected Error Occurred</h2>
+// <p><strong>${e.message}</strong></p>
+// <p>
+//   Please report this at
+//   <a href="https://github.com/sql-formatter-org/sql-formatter/issues">Github issues page.<a>
+// </p>
+// <p>Stack Trace:</p>
+// <pre>${e.stack.toString()}</pre>
+// `
+//                 );
+                showOutput2("SQL语法错误:\n\n" + input.value);
+
+            }
+        }
+    }
+
+    input.addEventListener('input', format);
+    [
+        language,
+        tabWidth,
+        useTabs,
+        keywordCase,
+        indentStyle,
+        logicalOperatorNewline,
+        tabulateAlias,
+        commaPosition,
+        expressionWidth,
+        lineBetweenQueries,
+        denseOperators,
+        newlineBeforeSemicolon,
+    ].forEach(option => option.addEventListener('change', format));
+
+    format();
 };
 
 document.addEventListener('DOMContentLoaded', attachFormat);
